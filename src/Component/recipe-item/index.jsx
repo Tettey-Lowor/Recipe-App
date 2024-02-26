4d4f951b92ebe7f0cc7827aea3003ec54ea61f63
Tettey-Lowor <frederickatettey10@gmail.com>
2024-02-26 12:13:50 +0000

    fetching recipes from API

---
+++ src/Component/recipe-item/index.jsx
@@ -0,0 +1,23 @@
+import { Card, Grid, CardMedia, CardContent, Typography } from "@mui/material";
+
+export default function RecipeItem({title, image}) {
+   
+    return (
+        <Grid item xs={4}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            sx={{ height: 140 }}
+                            image={image}
+                            title="green iguana"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {title}
+                            </Typography>
+                            
+                        </CardContent>
+                        
+                    </Card>
+                </Grid>
+    );
+}
